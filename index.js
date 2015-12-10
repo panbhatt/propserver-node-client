cbfb5d8b7d05ad966ba245e6094aa8fb14331afa
panbhatt <panbhatt@gmail.com>
2015-12-10 10:59:33 -0500

    Added index.js and test.js files

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var sentinel = require('request');
+
+module.exports = function(config) {
+  return new PropManagerClient(config);
+};
+
+
+// Prop Manager client object that contains all the functionalities to be invoked from the client side.
+function PropManagerClient(config) {
+
+
+
+
+}
